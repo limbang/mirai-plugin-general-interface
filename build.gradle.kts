ef1e1f3d32f9a7a7f1c786ceb0a94c6d91fc61e3
limbang <495071565@qq.com>
2022-06-10 16:09:16 +0800

    build: 添加发布到 sonatype

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "top.limbang"
-version = "0.0.1"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
@@ -16,8 +16,23 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
 publishing {
     repositories {
+        maven {
+            name = "OSSRH"
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
         maven {
             name = "GitHubPackages"
             url = uri("https://maven.pkg.github.com/limbang/mirai-plugin-general-interface")
@@ -29,7 +44,26 @@ publishing {
     }
     publications {
         create<MavenPublication>("gpr") {
+            artifactId = "mirai-plugin-general-interface"
             from(components["java"])
+            pom {
+                licenses {
+                    licenses {
+                        name.set("GNU Affero General Public License v3.0")
+                        url.set("https://choosealicense.com/licenses/agpl-3.0/")
+                    }
+                    developers {
+                        developer {
+                            name.set("limbang")
+                            email.set("495071565@qq.com")
+                        }
+                    }
+                    scm {
+                        connection.set("git@github.com:limbang/mirai-plugin-general-interface.git")
+                        url.set("https://github.com/limbang/mirai-plugin-general-interface")
+                    }
+                }
+            }
         }
     }
 }
